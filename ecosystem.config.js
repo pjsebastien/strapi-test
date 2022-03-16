142e06a4cf447a6b156d81fb107b687c24a313d7
pierrejean <pj.sebastien@gmail.com>
2022-03-16 09:21:36 +0400

    prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,11 @@ module.exports = {
 
   deploy: {
     production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
+      user: "ubuntu",
+      host: "51.254.116.139",
+      ref: "origin/main",
+      repo: "https://github.com/pjsebastien/strapi-test.git",
+      path: "/home/ubuntu",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
